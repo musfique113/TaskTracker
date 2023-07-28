8309f54f35cceac00adea6a448798a1a53560902
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-28 16:55:58 +0600

    succesfully designed the Login UI , add the password visibility button

--- lib/ui/presentation/screens/login_screen.dart
+++ lib/ui/presentation/screens/login_screen.dart
@@ -1,15 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanager_ostad/ui/presentation/widgets/screen_background.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+
+  // LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _passwordVisible = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ScreenBackground(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Get Started with",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 36,
+                      letterSpacing: 0.6),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+            TextField(
+              obscureText: !_passwordVisible,
+              decoration: InputDecoration(
+                hintText: "Password",
+                suffixIcon: IconButton(
+                  icon: _passwordVisible
+                      ? Icon(Icons.visibility)
+                      : Icon(Icons.visibility_off),
+                  onPressed: () {
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.arrow_forward_ios)),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Forgot Password",
+                        style: TextStyle(color: Colors.grey),
+                      )),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400,letterSpacing: 0.3),),
+                    TextButton(onPressed: () {}, child: const Text("Sign Up"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
 }
