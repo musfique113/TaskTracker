01398b838fe09ddf9ca15269b6b474a3b20006fb
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-31 00:39:28 +0600

    added image picker functionality

--- lib/ui/presentation/screens/edit_profile_screen.dart
+++ lib/ui/presentation/screens/edit_profile_screen.dart
@@ -1,14 +1,106 @@
-import 'package:flutter/material.dart';
-import 'package:taskmanager_ostad/ui/presentation/widgets/screen_background.dart';
+import 'dart:io';
 
-class EditProfileScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:taskmanager_ostad/ui/presentation/widgets/screen_background.dart';
+import 'package:taskmanager_ostad/ui/presentation/widgets/user_profile_banner.dart';
+
+class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
+
+  @override
+  _EditProfileScreenState createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  File? _image;
+
+  Future<void> _getImageFromGallery() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
         child: Column(
+          children: [
+            UserProfileBanner(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 16,),
+                  Text(
+                    'Update Profile',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge,
+                  ),
+                  const SizedBox(height: 16,),
+                  _image != null
+                      ? Image.file(_image!, height: 50, width: 50, fit: BoxFit.cover,)
+                      : ElevatedButton(
+
+                    onPressed: _getImageFromGallery,
+                    child: Icon(Icons.add_a_photo),
+                  ),
+                  const SizedBox(height: 8,),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'First Name',
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Last Name',
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: 'Mobile',
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                  TextFormField(
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(height: 16,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle form submission
+                      },
+                      child: const Icon(Icons.arrow_circle_right_outlined),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
 
         ),
       ),
