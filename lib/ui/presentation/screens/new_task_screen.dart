36a655d21bde91922d3cb356f698759d62f80084
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-30 21:34:10 +0600

    fix the listTile problem

--- lib/ui/presentation/screens/new_task_screen.dart
+++ lib/ui/presentation/screens/new_task_screen.dart
@@ -16,48 +16,74 @@ class NewTaskScreen extends StatelessWidget {
                 Expanded(
                   child: SummaryCard(
                     title: "Canceled",
-                      number: 123,
+                    number: 123,
                   ),
                 ),
                 Expanded(
                   child: SummaryCard(
                     title: "Completed",
-                      number: 123,
+                    number: 124,
                   ),
                 ),
                 Expanded(
                   child: SummaryCard(
                     title: "Progress",
-                      number: 123,
+                    number: 125,
                   ),
                 ),
                 Expanded(
                   child: SummaryCard(
                     title: "New Task",
-                      number: 123,
+                    number: 126,
                   ),
                 ),
-                Expanded(child: ListView.builder(
-                    itemCount: 20,
-                    itemBuilder: (context,index){
-                      return ListTile(
-                        title: Text("Title"),
-                        subtitle:Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Sub title"),
-                            Text("Date"),
-                            Row(
-                              children: [
-                                Chip(label: Text("New"),backgroundColor: Colors.blue,)
-                              ],
-                            )
-                          ],
-                        ) ,
-                      );
-                    },),
-                ),
               ],
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                 return ListTile(
+                    title: const Text('Title will be here'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Title will be here'),
+                        const Text('Date'),
+                        Row(
+                          children: [
+                            const Chip(
+                              label: Text(
+                                'New',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              backgroundColor: Colors.blue,
+                            ),
+                            const Spacer(),
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.delete_forever_outlined,
+                                  color: Colors.red.shade300,
+                                )),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.green,
+                                )),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Divider(
+                    height: 4,
+                  );
+                },
+              ),
             ),
           ],
         ),
@@ -65,4 +91,3 @@ class NewTaskScreen extends StatelessWidget {
     );
   }
 }
-
