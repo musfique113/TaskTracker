81c221b9b76a9b371b7196cb1ebe9b47d8827c71
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-08-01 17:53:20 +0600

    aded post response

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart';
-import 'package:taskmanager_ostad/data/models/response_object.dart';
+import 'package:taskmanager_ostad/data/models/network_response.dart';
 
 class NetworkCaller {
   Future<NetworkResponse> getRequest(String url) async {
@@ -19,5 +19,18 @@ class NetworkCaller {
     return NetworkResponse(false,-1, null);
   }
 
+  Future<NetworkResponse> postRequest(String url,Map<String, dynamic>body) async {
+    try {
+      Response response = await post(Uri.parse(url), body: jsonEncode(body));
+      if (response.statusCode == 200) {
+        return NetworkResponse(true,response.statusCode, jsonDecode(response.body));
+      } else {
+        return NetworkResponse(false,response.statusCode, null);
 
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false,-1, null);
+  }
 }
