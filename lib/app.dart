4aef040e120872b9ec3bfef509e187ba14e74b39
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-08-02 18:01:35 +0600

    added logic for unauthorized token , unauthorized user cant create new task ad getLogged out

--- lib/app.dart
+++ lib/app.dart
@@ -3,12 +3,19 @@ import 'package:taskmanager_ostad/ui/presentation/screens/splash_screen.dart';
 import 'package:taskmanager_ostad/ui/theme_data/dark_theme.dart';
 import 'package:taskmanager_ostad/ui/theme_data/light_theme.dart';
 
-class TaskManagerApp extends StatelessWidget {
+class TaskManagerApp extends StatefulWidget {
+  static GlobalKey<ScaffoldState> globalKey = GlobalKey<ScaffoldState>();
   const TaskManagerApp({super.key});
 
   @override
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      key: TaskManagerApp.globalKey,
       debugShowCheckedModeBanner: false,
       title: 'Task Manager',
       theme: AppLightTheme.lightTheme,
--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -1,13 +1,17 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'dart:developer';
 import 'package:http/http.dart';
+import 'package:taskmanager_ostad/app.dart';
 import 'package:taskmanager_ostad/data/models/auth_utility.dart';
 import 'package:taskmanager_ostad/data/models/network_response.dart';
+import 'package:taskmanager_ostad/ui/presentation/screens/auth/sign_in_screen.dart';
 
 class NetworkCaller {
   Future<NetworkResponse> getRequest(String url) async {
     try {
-      Response response = await get(Uri.parse(url));
+      Response response = await get(Uri.parse(url),
+          headers: {'token': AuthUtility.userInfo.token.toString()});
       if (response.statusCode == 200) {
         return NetworkResponse(
             true, response.statusCode, jsonDecode(response.body));
@@ -20,17 +24,22 @@ class NetworkCaller {
     return NetworkResponse(false, -1, null);
   }
 
-  Future<NetworkResponse> postRequest(
-      String url, Map<String, dynamic> body) async {
+  Future<NetworkResponse> postRequest(String url,
+      Map<String, dynamic> body) async {
     try {
       Response response = await post(Uri.parse(url),
-          headers: {'Content-type': 'application/json', 'token' : AuthUtility.userInfo.token.toString() },
+          headers: {
+            'Content-type': 'application/json',
+            'token': AuthUtility.userInfo.token.toString()
+          },
           body: jsonEncode(body));
       log(response.statusCode.toString());
       log(response.body);
       if (response.statusCode == 200) {
         return NetworkResponse(
             true, response.statusCode, jsonDecode(response.body));
+      } else if (response.statusCode == 401) {
+        goToLoginIfUnauthorized();
       } else {
         return NetworkResponse(false, response.statusCode, null);
       }
@@ -38,5 +47,13 @@ class NetworkCaller {
       log(e.toString());
     }
     return NetworkResponse(false, -1, null);
+  }
+
+  Future<void> goToLoginIfUnauthorized() async {
+    await AuthUtility.clearUserInfo();
+    Navigator.pushAndRemoveUntil(
+        TaskManagerApp.globalKey.currentState!.context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+            (route) => false);
   }
 }
